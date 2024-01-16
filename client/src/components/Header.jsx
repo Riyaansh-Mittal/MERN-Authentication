c766df0696af5304ffac38c91e3217685fec10dd
Riyaansh-Mittal <riyaanshmittal14@gmail.com>
2024-01-16 12:27:19 +0530

    Create header component and add 'jsx' to tailwind file to make sure that jsx files also work fine with app.js

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='bg-slate-400'>
+      <div className='flex justify-between items-center max-w-6cl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold'>Auth App</h1>
+        </Link>
+        <ul className='flex gap-3'>
+          <Link to='/'>
+            <li>Home</li>
+          </Link>
+          <Link to='/About'>
+          <li>About</li>
+          </Link>
+          <Link to='/sign-in'>
+          <li>Sign In</li>
+          </Link>
+        </ul>
+      </div>
+      
     </div>
   )
 }
