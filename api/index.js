08dcff6e3f8a661b00ccbc164659645cac375af7
Riyaansh-Mittal <riyaanshmittal14@gmail.com>
2024-01-16 14:34:59 +0530

    add catch statement to mongoose.connect

--- api/index.js
+++ api/index.js
@@ -3,8 +3,12 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose.connect(process.env.MONGO)
+.then(() => {
     console.log('Connected to MongoDB')
+})
+.catch((err) => {
+    console.log(err)
 })
 
 const app = express();
